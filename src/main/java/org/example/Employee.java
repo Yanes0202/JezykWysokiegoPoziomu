e3a80147f7f90ed2ecf9a81cb541bd4f8af32e45
Yanes <adriand0202@gmail.com>
2023-10-22 12:38:33 +0200

    Employee Queue added

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class Employee {
+    private String name;
+    private int salary;
+    private boolean isMale;
+}
+
+class Staff {
+    private List<Employee> employeeList = new ArrayList<>();
+
+    void addEmployee(Employee employee) {
+            if(employeeList.size() < 99) {
+                employeeList.add(employee);
+            } else {
+                throw new ArrayIndexOutOfBoundsException("Za duża ilość pracowników");
+            }
+    }
+
+    Double avgSalary() {
+        if(!employeeList.isEmpty()) {
+            return employeeList.stream().mapToInt(Employee::getSalary).average().getAsDouble();
+        } else
+            return null;
+    }
+
+    Double avgSalaryMale() {
+        if (!employeeList.isEmpty()) {
+            return employeeList.stream().filter(Employee::isMale).mapToInt(Employee::getSalary).average().getAsDouble();
+        } else
+            return null;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Łukasz",10000, true);
+        Employee employee2 = new Employee("Marlena",5000, false);
+        Staff staff = new Staff();
+        staff.addEmployee(employee1);
+        staff.addEmployee(employee2);
+        System.out.println(staff.avgSalary());
+        System.out.println(staff.avgSalaryMale());
+    }
+}
---
+++ src/main/java/org/example/Queue.java
@@ -0,0 +1,84 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Queue extends collection implements pushpop {
+    private int index = 0;
+    int popIndex = 0;
+    public static void main(String[] args) {
+        Queue queue = new Queue(5);
+        Stack stack = new Stack(6);
+
+
+        for(int i=0; i<7; i++) {
+            queue.push(i);
+            stack.push(i);
+        }
+
+        for(int i =0; i<4; i++) {
+            System.out.println("queue: " + queue.pop());
+        }
+        for (int i = 0; i <4; i++) {
+            System.out.println("stack: " +  stack.pop());
+        }
+
+        System.out.println(Arrays.toString(stack.array));
+        System.out.println(Arrays.toString(queue.array));
+    }
+
+    Queue(int i) {
+        array = new int[i];
+    }
+    @Override
+    public int pop() {
+        int result = array[0];
+        for(int i = 0; i<array.length; i++) {
+            if(i+1<array.length) {
+                array[i] = array[i+1];
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void push(int item) {
+        if(index < array.length) {
+            array[index] = item;
+            index++;
+        } else
+            System.out.println("full");
+    }
+}
+
+class Stack extends collection implements pushpop {
+    private int index = 0;
+    Stack(int i) {
+        array = new int[i];
+    }
+    @Override
+    public int pop() {
+        int result = array[index-1];
+        index --;
+        return result;
+    }
+
+    @Override
+    public void push(int item) {
+        if(index < array.length) {
+            array[index] = item;
+            index++;
+        } else
+            System.out.println("full");
+    }
+}
+
+class collection {
+    public int[] array;
+}
+
+interface pushpop {
+    int pop();
+    void push(int item);
+}
+
+
