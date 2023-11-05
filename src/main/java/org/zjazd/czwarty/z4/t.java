2a49ebc2b54c8f5f18b0d7060083dc58a21a2afb
Adrian <adriand0202@gmail.com>
2023-11-05 10:25:02 +0100

    zad 5

--- src/main/java/org/zjazd/czwarty/z4/t.java
+++ src/main/java/org/zjazd/czwarty/z4/t.java
@@ -1,22 +1,74 @@
 package org.zjazd.czwarty.z4;
-
+    //1
+/**
 public class t <T> {
     T a;
     T[] b;
-    t(T t) {
+//    t(T t) { a = t; }
+    T getA() { return a;}
+}
+ */
+
+    //2
+/**
+class u <U,T> extends t <T> {
+}
+
+class u <U,V> extends t <Integer> {
+}
+ */
+
+
+    //2
+/**class t2 {
+    Object a;
+    Object[] b;
+
+    t2(Object t) {
         a = t;
     }
-    T getA() { return a;}
+    Object getA() {
+        return a;
+    }
+}*/
+
+//3
+/**
+public interface t <T>{
+}
+
+class u <U,T> implements t <T> {
+
+}
+ */
+
+    //4
+class t {
+    <T> T f(T t) {return t;}
 }
 
 class Main {
     public static void main(String[] args) {
+        //1
+/**
         t<Integer> t1 = new t<>(5);
         System.out.println(t1.getA());
 
         t<String> t2 = new t<>("abc");
         System.out.println(t2.getA());
+*/
 
+        //2
         // można też zamiast typu generycznego wykorzysztać klasę Object
+/**
+        t2 i = new t2(1);
+        Integer j = (Integer) i.getA();
+
+        t2 s = new t2("abc");
+        String w = (String) s.getA();
+ */
+        //3
+//        u<Integer, String> u = new u<>();
     }
 }
+
